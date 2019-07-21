d56f2ade066d78982533137c7c1827531d951a37
Juan Carlos Amor <49997664+Jcamor@users.noreply.github.com>
2019-07-21 17:44:00 +0200

    Add files via upload

---
+++ aprendiendo-php/03-variables/index.php
@@ -0,0 +1,10 @@
+<?php
+
+    // uso de variables
+    $mi_primera_variable="Hola Mundo desde una variable";
+    echo "<h1>".$mi_primera_variable."</h1>";
+
+    $numero=44;
+    $verdadero=true;
+
+    echo $numero."<br>";
