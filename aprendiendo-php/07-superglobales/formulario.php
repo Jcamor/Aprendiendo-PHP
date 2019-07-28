d57afab7193b4c0a7fbf61424e19a881c726efeb
Juan Carlos Amor <jcamorgutierrez@gmail.com>
2019-07-28 19:55:17 +0200

    Carpeta 7

--- aprendiendo-php/07-superglobales/formulario.php
+++ aprendiendo-php/07-superglobales/formulario.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Formulario en PHP</title>
+    <title>Formulario en PHP para el envio por POST y GET</title>
 </head>
 
 <body>
